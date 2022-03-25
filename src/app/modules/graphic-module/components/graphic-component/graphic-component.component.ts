011d8b0c8b4889f42c62596aa2a56bcc1a49e48b
Osvaldo <yair140193@gmail.com>
2022-03-25 17:06:18 -0600

    Insert format of numbers inside of chart

--- src/app/modules/graphic-module/components/graphic-component/graphic-component.component.ts
+++ src/app/modules/graphic-module/components/graphic-component/graphic-component.component.ts
@@ -35,7 +35,8 @@ export class GraphicComponentComponent implements OnInit {
       cutout: '94%',
       responsive: true,
       animation:{ onComplete: function() {
-        me.doit(ctx);
+        me.title(ctx);
+        me.subTitle(ctx)
       }}
     }
   }
@@ -62,8 +63,28 @@ export class GraphicComponentComponent implements OnInit {
     }
   }
 
-  doit(ctx) {
-    console.log(ctx)
+  title(ctx) {
+    let width = this.canvas.nativeElement.clientWidth,
+        height = this.canvas.nativeElement.clientHeight;
+        console.log(width, height)
+
+    /* Configuracion de fuente */
+    let fontSize = (height / 150).toFixed(1);
+    ctx.font = fontSize + "em Verdana";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = "#4f4f4f";
+
+    /* Posicionamieto de texto en la grafica */
+    let text = `${ this.metric.title.toUpperCase() }`,
+        textX = Math.round((width - ctx.measureText(text).width) / 2),
+        textY = height -160;
+
+    ctx.fillText(text, textX, textY);
+    ctx.restore();
+        
+  }
+      
+  subTitle(ctx){
     let width = this.canvas.nativeElement.clientWidth,
         height = this.canvas.nativeElement.clientHeight;
         console.log(width, height)
@@ -74,19 +95,14 @@ export class GraphicComponentComponent implements OnInit {
     ctx.textBaseline = "middle";
     ctx.fillStyle = "#d7d7d7";
 
-    /* Posicionamieto de texto en la grafica */
-    let text = `${this.metric.title.toUpperCase()}`,
-        textX = Math.round((width - ctx.measureText(text).width) / 2),
-        textY = height -160;
-
-    let text2 = `${this.metric.total}`,
+    let text2 = `${ this.metric.title == 'Revenue' ? new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(this.metric.total) : Intl.NumberFormat("de-DE").format(this.metric.total) }`,
     text2X = Math.round((width - ctx.measureText(text2).width) / 2),
     text2Y = height -120;
 
-    ctx.fillText(text, textX, textY);
-    ctx.restore();
-
     ctx.fillText(text2, text2X, text2Y);
-    /* ctx.restore(); */
-   }
+    ctx.restore();
+  }
+
+
+
 }
